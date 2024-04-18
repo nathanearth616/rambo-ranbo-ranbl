d98e3d944638e8d875dcb81f744dd4c697bfde8f
nathanearth616 <wrkclf@gmail.com>
2024-04-18 13:38:04 -0800

    fix sql injection bug

--- ktor-sample/src/main/kotlin/com/example/UserAuth.kt
+++ ktor-sample/src/main/kotlin/com/example/UserAuth.kt
@@ -2,15 +2,16 @@ import java.sql.DriverManager
 import java.sql.ResultSet
 import org.mindrot.jbcrypt.BCrypt
 
-fun signUp(username: String, password: String, email: String) {
-    val url = "jdbc:postgresql://localhost:5432/usr_cred"
-    val user = "postgres"
-    val password = "hitmanagent47"
 
+val url = "jdbc:postgresql://localhost:5432/usr_cred"
+val user = "postgres"
+val password = "hitmanagent47"
+
+fun signUp(username: String, password: String, email: String) {
     val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
 
     DriverManager.getConnection(url, user, password).use { conn ->
-        conn.prepareStatement("INSERT INTO user_credentials (username, password) VALUES (?, ?)").use { stmt ->
+        conn.prepareStatement("INSERT INTO user_credentials (username, password, email) VALUES (?, ?, ?)").use { stmt ->
             stmt.setString(1, username)
             stmt.setString(2, hashedPassword)
             stmt.setString(3, email)
@@ -20,18 +21,17 @@ fun signUp(username: String, password: String, email: String) {
 }
 
 fun login(username: String, password: String): Boolean {
-    val url = "jdbc:postgresql://localhost:5432/usr_cred"
-    val user = "postgres"
-    val password = "hitmanagent47"
-
     DriverManager.getConnection(url, user, password).use { conn ->
-        conn.prepareStatement("SELECT * FROM user_credentials WHERE username = ? AND password = ?").use { stmt ->
+        conn.prepareStatement("SELECT password FROM user_credentials WHERE username = ?").use { stmt ->
             stmt.setString(1, username)
-            stmt.setString(2, password)
             val resultSet: ResultSet = stmt.executeQuery()
-            return resultSet.next()
+            if (resultSet.next()) {
+                val hashedPassword = resultSet.getString("password")
+                return BCrypt.checkpw(password, hashedPassword)
+            }
         }
     }
+    return false
 }
 
 

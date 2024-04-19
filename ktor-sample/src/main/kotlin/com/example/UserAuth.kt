aa7ecef3cf0d2818a6158d0b354ee3ab48ec6ec3
nathanearth616 <wrkclf@gmail.com>
2024-04-19 00:47:14 -0800

    add comments

--- ktor-sample/src/main/kotlin/com/example/UserAuth.kt
+++ ktor-sample/src/main/kotlin/com/example/UserAuth.kt
@@ -2,14 +2,17 @@ import java.sql.DriverManager
 import java.sql.ResultSet
 import org.mindrot.jbcrypt.BCrypt
 
-
+// Database connection details
 val url = "jdbc:postgresql://localhost:5432/usr_cred"
 val user = "postgres"
 val password = "hitmanagent47"
 
+// Function to register a new user
 fun signUp(username: String, password: String, email: String) {
+    // Hash the provided password using BCrypt
     val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt())
 
+    // Establish a connection to the database and insert user credentials
     DriverManager.getConnection(url, user, password).use { conn ->
         conn.prepareStatement("INSERT INTO user_credentials (username, password, email) VALUES (?, ?, ?)").use { stmt ->
             stmt.setString(1, username)
@@ -20,17 +23,21 @@ fun signUp(username: String, password: String, email: String) {
     }
 }
 
+// Function to authenticate a user
 fun login(username: String, password: String): Boolean {
     DriverManager.getConnection(url, user, password).use { conn ->
+        // Prepare SQL statement to select hashed password based on username
         conn.prepareStatement("SELECT password FROM user_credentials WHERE username = ?").use { stmt ->
             stmt.setString(1, username)
             val resultSet: ResultSet = stmt.executeQuery()
             if (resultSet.next()) {
-                val hashedPassword = resultSet.getString("password")
+                val hashedPassword = resultSet.getString("password")// Retrieve hashed password from database
+                // Check if the provided password matches the hashed password using BCrypt
                 return BCrypt.checkpw(password, hashedPassword)
             }
         }
     }
+    // Return false if the user authentication fails
     return false
 }
 
